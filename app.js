70b398a1b1b57d4dfa6c6d9abf07ae01a25aa070
Eric Yang <eric@MacBook-Pro-2.local>
2019-03-19 18:48:40 -0700

    finished lab 4

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let WebSocket = require('ws');
 
 
-let wss = new WebSocket.Server({port: process.env.PORT || 8080});
+let wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
 
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
